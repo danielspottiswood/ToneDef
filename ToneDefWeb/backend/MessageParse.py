c12de4862477a59db5f145e4dfad40d87512d536
Daniel Spottiswood <Danielspottiswood@gmail.com>
2017-10-28 16:49:14 -0400

    Added an API call to IBM Web Services Tone Analyzer.

--- ToneDefWeb/backend/MessageParse.py
+++ ToneDefWeb/backend/MessageParse.py
@@ -2,9 +2,18 @@ __author__ = 'Andrew'
 
 import random
 import urllib
+import json
+import watson_developer_cloud
+from watson_developer_cloud import ToneAnalyzerV3
+from os.path import join, dirname
+
+tonedef = watson_developer_cloud.ToneAnalyzerV3(
+    '2017-10-27',
+    username= "f0d16349-4f18-4669-9fff-a53ad63fa956",
+    password='UvaNFrrMqnlH')
 
 messages = open('Lucas', 'r+')
-convo = []
+convo = ""
 
 switch = 0;
 for line in messages:
@@ -14,7 +23,7 @@ for line in messages:
         person = line[:ind]
         mess = line[ind+2:]
         mess = mess[:-1]
-        convo.append(mess)
+        convo = convo + mess
         print(person)
         print(mess)
     else:
@@ -23,12 +32,16 @@ for line in messages:
         hour = line[ind-1:ind]
         minute = line[ind+1:ind+3]
         print(hour + ':' + minute)
-        print
-
 print convo
 
+print("\ntone_chat() example 1:\n")
+utterances = [{'text': 'I am very happy.', 'user': 'glenn'},
+              {'text': 'It is a good day.', 'user': 'glenn'}]
+print(json.dumps(tonedef.tone_chat(utterances), indent=2))
 
 
+with open(join(dirname(__file__), 'tone.json')) as tone_json:
+  tone = tonedef.tone(json.load(tone_json)[convo], tones='emotion',
+    content_type='text/plain')
 
-
-
+print(json.dumps(tone, indent=2))
